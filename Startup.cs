010179c6753933e31e5df3341bdaf1a26b3c4be7
TAKIEDONS <105198883+TAKIEDONS@users.noreply.github.com>
2023-01-17 12:02:27 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC_EMMACULATE.Startup))]
+namespace ASP.NET_MVC_EMMACULATE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
